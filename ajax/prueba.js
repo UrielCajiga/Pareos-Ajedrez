6d47d4ce9c9d315e3c4f9e774cf3fa066f7946da
UrielCajiga <31425133+UrielCajiga@users.noreply.github.com>
2020-05-06 21:29:31 -0500

    Add files via upload

--- ajax/prueba.js
+++ ajax/prueba.js
@@ -2,42 +2,22 @@ $(document).ready(function(){
 	console.log("Hola desde la prueba");
 
 	var usuario = {"nombre":"Uriel",
-			"apellido":"Cajiga"}
+			"apellido":"Cajiga"};
 
 	//console.log(usuario);
-	console.log("Este es el get")
-	$.get("https://reqres.in/api/users/2",function(response){
-		console.log(response);
-	});
 
-	console.log("Este es el POST")
+
+	console.log("Este es el POST");
 	$.ajax({
 		type:"POST",
 		url:"servidor.php",
-		data:{"nombre":"Uriel",
-			"apellido":"Cajiga"},
+		data:usuario,
 		success:function(r){
 			var us = JSON.parse(r);
 			console.log(us);
-			us.map(function(num){
-				console.log(num);
-			});
 		}
 	});
 
-	$.ajax({
-		type:"POST",
-		url:"servidor.php",
-		data:{"nombre":"Osvaldo",
-			"apellido":"Gutierrez"},
-		success:function(r){
-			var us = JSON.parse(r);
-			console.log(us);
-			us.map(function(num){
-				console.log(num);
-			});
-		}
-	});
 
 });
 
--- ajax/servidor.php
+++ ajax/servidor.php
@@ -1,12 +1,36 @@
 <?php
 
-$usuario = $_POST;
-$arreglo = array();
+$nom = $_POST['nombre'];
+$ape = $_POST['apellido'];
 
-//for ($x = 1; $x <= 10; $x++){
 
-    array_push($arreglo, $usuario);
-//}
+//echo json_encode($nom.' '.$ape);
+//echo json_encode($ape);
 
-echo json_encode($arreglo);
+
+try{
+	/*
+	$base = new PDO('mysql:host=localhost; dbanme = pareos', 'root','');
+	//echo json_encode('Conexcion OK');
+	$base->exec("SET CHARACTER SET utf8");
+	$sql = 'SELECT `nombre`, `apellido` FROM `jugadores` WHERE nombre = ?"Andres"';
+	$resultado = $base->prepare($sql);
+
+	$resultado->execute(array("Andres"));
+	*/
+	$conexcion = mysqli_connect('localhost','root','','pareos');
+	$sql = 'SELECT nombre, apellido FROM jugadores';
+	$res = mysqli_query($conexcion,$sql);
+	$envio = array();
+ 	while($consulta = mysqli_fetch_array($res)){
+ 		array_push($envio, $consulta); 
+ 	}
+ 	echo json_encode($envio);
+	
+
+}catch(Exception $e){
+	die('Error: ' . $e->GetMessage());
+}
+
 ?>
+  
